b570fd72cc4f4c3a796817715625d1eef9ac7f4d
ningmeng <hyoobo@gmail.com>
2022-08-07 14:36:58 +0800

    feat: return all newline

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import pdfplumber
 
+#翻译摘要就算成功...
+
+def all_newline(page_number=1):
+    newline=[0,1]
+    file_path = r'test.pdf'
+    with pdfplumber.open(file_path) as pdf:
+        all_page_number = len(pdf.pages[:])
+        if page_number==1:
+            while newline[-1] !=-1:
+                page0_1 = str(pdf.pages[0].extract_text())
+                newline.append(page0_1.find('\n', newline[-1]+1))
+                a=page0_1.find('\n', newline[-1]+1)
+            del(newline[0])
+            del (newline[0])
+            del (newline[-1])
+            return newline
+        else:
+            #TODO 完善根据页码返回功能，目前默认第一页
+            pass
+#返回了所有换行符
 
 def main():
     file_path = r'test.pdf'
@@ -24,7 +44,8 @@ def main():
         # TODO 查找一行一个或两个的单词
         o_abstract = page0_1[start_abstract + 16:end_abstract]
         while 1:
-            double_space=o_abstract.find(' ')
+            double_space=o_abstract.find('  ')
+            break
 
         # TODO 查找多余一个的空格，并删除所在位置与前一个空格之间的单词
 
@@ -34,4 +55,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    all_newline()
